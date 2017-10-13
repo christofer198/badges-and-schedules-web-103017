d5d4181518aa534d6a3b0a502e82ab4cd9be1645
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-13 23:02:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  room_array = [1,2,3,4,5,6,7]
+  for each_name in array
+    for each_room in room_array
+      puts "Hello #{each}. You'll be assigned to room #{each_room}"
+    end
+  end
+end
