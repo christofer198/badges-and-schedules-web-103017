1f321b508d1743c3df61eaa01c0485f560e9c0c9
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-13 23:08:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-
+  new_array = []
   for each in array
-    return "Hello, my name is #{each}."
+    new_array.push("Hello, my name is #{each}.")
   end
+  return new_array
 end
